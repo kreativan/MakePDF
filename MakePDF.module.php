f4924081b0604ee262ef51e141e64dc67112dbf9
Ivan Milincic <kreativan.dev@gmail.com>
2023-09-25 14:46:29 +0300

    init upload

---
+++ MakePDF.module.php
@@ -0,0 +1,311 @@
+<?php
+
+/**
+ * MakePDF
+ * @author Ivan Milincic <hello@kreativan.dev>
+ * @link http://www.kraetivan.dev
+ */
+
+namespace ProcessWire;
+
+class MakePDF extends WireData implements Module {
+
+  public static function getModuleInfo() {
+    return array(
+      'title' => 'MakePDF',
+      'version' => 100,
+      'summary' => 'Wrapper around mPDF library for ProcessWire',
+      'icon' => 'file-pdf-o',
+      'author' => "Ivan Milincic",
+      "href" => "https://kreativan.dev",
+      'singular' => true,
+      'autoload' => true
+    );
+  }
+
+  public function __construct() {
+    // ...
+  }
+
+
+  public function init() {
+    /**
+     * Temp folder. Create temp folder if not exists
+     */
+    $this->temp_folder = $this->config->paths->assets . 'temp/';
+    if (!is_dir($this->temp_folder)) $this->files->mkdir($this->temp_folder);
+  }
+
+  public function test() {
+    return "MakePDF module is working!";
+  }
+
+  /**
+   * Get temp folder path
+   * @return string
+   */
+  public function temp_path() {
+    return $this->temp_folder;
+  }
+
+  /**
+   * Default PDF options
+   * @param string $html - html content to convert
+   * @param string $options['mode'] - utf-8, iso-8859-1, etc
+   * @param string $options['format'] - page  dimensions: [210, 297]
+   * @param string $options['orientation'] - P or L - portrait or landscape
+   * 
+   * @param int $options['margin_top'] - default 20
+   * @param int $options['margin_bottom'] - default 20
+   * @param int $options['margin_left'] - default 20
+   * @param int $options['margin_right'] - default 20
+   * @param int $options['margin_header'] - default 20
+   * @param int $options['margin_footer'] - default 20
+   * 
+   * @param string $options['output'] - INLINE, FILE, DOWNLOAD
+   * @param string $options['dest'] - for 'FILE' output method
+   * @param string $options['file_name'] - for 'FILE' output method
+   * 
+   * @param string $options['header'] - html header
+   * @param string $options['footer'] - html footer
+   * 
+   * @param string $options['font'] - sans, condensed, serif, slab
+   */
+  public function options($array = []) {
+
+    $options = [
+      'mode' => !empty($array['mode']) ? $array['mode'] : "utf-8",
+      'format' => !empty($array['format']) ? $array['format'] : [210, 297],
+      'orientation' => !empty($array['orientation']) ? $array['orientation'] : "P",
+      'margin_top' => !empty($array['margin_top']) ? $array['margin_top'] : 20,
+      'margin_bottom' => !empty($array['margin_bottom']) ? $array['margin_bottom'] : 20,
+      'margin_left' => !empty($array['margin_left']) ? $array['margin_left'] : 20,
+      'margin_right' => !empty($array['margin_right']) ? $array['margin_right'] : 20,
+      'margin_header' => !empty($array['margin_header']) ? $array['margin_header'] : 20,
+      'margin_footer' => !empty($array['margin_footer']) ? $array['margin_footer'] : 20,
+      'output' => !empty($array['output']) ? $array['output'] : "INLINE",
+      'dest' => !empty($array['dest']) ? $array['dest'] : $this->temp_path(),
+      'file_name' => !empty($array['file_name']) ? $array['file_name'] : time(),
+      'header' => !empty($array['header']) ? $array['header'] : "",
+      'footer' => !empty($array['footer']) ? $array['footer'] : "",
+      'font' => !empty($array['font']) ? $array['font'] : "sans", // sans, cobdensed, serif, slab
+      'debug' => !empty($array['debug']) && $array['debug'] == 1 ? true : false,
+    ];
+
+    //
+    // Fonts
+    //
+
+    require_once(__DIR__ . "/mpdf/vendor/autoload.php");
+    $defaultConfig = (new \Mpdf\Config\ConfigVariables())->getDefaults();
+    $fontDirs = $defaultConfig['fontDir'];
+
+    $defaultFontConfig = (new \Mpdf\Config\FontVariables())->getDefaults();
+    $fontData = $defaultFontConfig['fontdata'];
+
+    $options['fontDir'] = array_merge($fontDirs, [
+      __DIR__ . '/fonts/',
+    ]);
+
+    $options['fontdata'] = $fontData + [ // lowercase letters only in font key
+      'sans' => [
+        'R' => 'roboto/Roboto-Regular.ttf',
+        'I' => 'roboto/Roboto-Italic.ttf',
+        'B' => 'roboto/Roboto-Bold.ttf',
+        'L' => 'roboto/Roboto-Lite.ttf',
+      ],
+      'condensed' => [
+        'R' => 'roboto-condensed/Roboto-Condensed-Regular.ttf',
+        'I' => 'roboto-condensed/Roboto-Condensed-Italic.ttf',
+        'B' => 'roboto-condensed/Roboto-Condensed-Bold.ttf',
+        'L' => 'roboto-condensed/Roboto-Condensed-Lite.ttf',
+      ],
+      'serif' => [
+        'R' => 'roboto-serif/Roboto-Serif-Regular.ttf',
+        'I' => 'roboto-serif/Roboto-Serif-Italic.ttf',
+        'B' => 'roboto-serif/Roboto-Serif-Bold.ttf',
+        'L' => 'roboto-serif/Roboto-Serif-Lite.ttf',
+      ],
+      'slab' => [
+        'R' => 'roboto-slab/Roboto-Slab-Regular.ttf',
+        'B' => 'roboto-slab/Roboto-Slab-Bold.ttf',
+        'L' => 'roboto-slab/Roboto-Slab-Lite.ttf',
+      ],
+      'mono' => [
+        'R' => 'roboto-mono/Roboto-Mono-Regular.ttf',
+        'I' => 'roboto-mono/Roboto-Mono-Italic.ttf',
+        'B' => 'roboto-mono/Roboto-Mono-Bold.ttf',
+        'L' => 'roboto-mono/Roboto-Mono-Lite.ttf',
+      ],
+    ];
+
+    $options['default_font'] = $options['font'];
+
+    // Add/override custom options
+    if (count($array) > 0) {
+      foreach ($array as $key => $val) $options[$key] = $val;
+    }
+
+    return $options;
+  }
+
+  /**
+   * HTML to PDF
+   * Simple converting html to pdf
+   * @param string $html - html content to convert
+   * @param array $options - array of options to set/override
+   * @see $this->options() for params
+   * @example $this->html2pdf($html, $options);
+   */
+  public function html2pdf($html, $params = []) {
+
+    // pdf options
+    $options = $this->options($params);
+
+    // params
+    $output     = $options['output'];
+    $dest       = $options['dest'];
+    $file_name  = $options['file_name'];
+    $header     = $options['header'];
+    $footer     = $options['footer'];
+
+    $options['margin-header'] = 0;
+
+    // mPDF
+    require_once(__DIR__ . "/mpdf/vendor/autoload.php");
+
+    //
+    // Init
+    //
+
+    $mpdf = new \Mpdf\Mpdf($options);
+
+    // Show image errors in debug mode
+    if ($this->config->debug) {
+      $mpdf->showImageErrors = true;
+    }
+
+    // Write html
+    $stylesheet = file_get_contents(__DIR__ . '/css/mpdf.css');
+    $mpdf->WriteHTML($stylesheet, \Mpdf\HTMLParserMode::HEADER_CSS);
+    if ($header != '') $mpdf->SetHTMLHeader($header);
+    if ($footer != '') $mpdf->SetHTMLFooter($footer);
+    $mpdf->WriteHTML($html);
+
+    // Output
+    if (!$output) {
+      return $mpdf->Output();
+    } elseif ($output == 'INLINE') { // display pdf in the browser - default
+      return $mpdf->Output($file_name . ".pdf", \Mpdf\Output\Destination::INLINE);
+    } elseif ($output == 'DOWNLOAD') { // trigger download pdf
+      return $mpdf->Output($file_name . ".pdf", \Mpdf\Output\Destination::DOWNLOAD);
+    } elseif ($output == "FILE") { // download file in specified path
+      return $mpdf->Output("{$dest}{$file_name}.pdf", \Mpdf\Output\Destination::FILE);
+    }
+  }
+
+  /**
+   * Generate multi-page PDF 
+   * 
+   * @param array $pdf_pages - array of pages to add
+   * In array you have to specify the path of the file that contains the html to be included in the pdf.
+   * You can pass any variable to the included file in same array.
+   * 
+   * @example
+   * $pdf_pages = [
+   *    "page_1" => [
+   *      "tmpl" => "path/to/template-page-1.php",
+   *      "some_var" => "some_value",
+   *    ],
+   *   "page_2" => [
+   *      "tmpl" => "path/to/template-page-2.php",
+   *      "some_var" => "some_value",
+   *   ],
+   * ];
+   * 
+   * @param array $pdf_files - array of static pdf files to add
+   * @example $pdf_files = ["file_1.pdf", "file_2.pdf"];
+   * 
+   * Override default PDF options
+   * @param array $options - array of options to set/override
+   * @see $this->options() for params
+   * 
+   * How to use?
+   * @example $this->generatePDF($pdf_pages, $pdf_files, $options);
+   */
+  public function generatePDF($pdf_pages = [], $pdf_files = [], $options_arr = []) {
+
+    // pdf options
+    $options = $this->options($options_arr);
+
+    // params
+    $output     = $options['output'];
+    $dest       = $options['dest'];
+    $file_name  = $options['file_name'];
+    $header     = $options['header'];
+    $footer     = $options['footer'];
+
+    // mPDF
+    require_once(__DIR__ . "/mpdf/vendor/autoload.php");
+
+    // Init
+    $mpdf = new \Mpdf\Mpdf($options);
+
+    // debug
+    if ($this->config->debug) {
+      $mpdf->showImageErrors = true;
+    }
+
+    // Write html
+    $stylesheet = file_get_contents(__DIR__ . '/css/mpdf.css');
+    $mpdf->WriteHTML($stylesheet, \Mpdf\HTMLParserMode::HEADER_CSS);
+
+    $i = 0;
+
+    // Add pages - from $pdf_pages array, by specifued (tmpl) template files
+    foreach ($pdf_pages as $item) {
+
+      $tmpl = !empty($item['tmpl']) ? $item['tmpl'] : false;
+      $data = [];
+      foreach ($item as $key => $value) $data[$key] = $value;
+      if ($tmpl && file_exists($tmpl)) {
+        if ($i++ > 0) $mpdf->AddPage();
+        ob_start();
+        $this->files->include($tmpl, $data);
+        $html = ob_get_contents();
+        ob_end_clean();
+        if ($header != '') $mpdf->SetHTMLHeader($header);
+        $mpdf->WriteHTML($html);
+        if ($footer != '') $mpdf->SetHTMLFooter($footer);
+      }
+    }
+
+    // Add static pdf files
+    if ($pdf_files && count($pdf_files) > 0) {
+      foreach ($pdf_files as $file) {
+        if (file_exists($file)) {
+          try {
+            $mpdf->AddPage();
+            $source_file = $mpdf->setSourceFile($file);
+            $pdf_file = $mpdf->importPage($source_file);
+            $mpdf->useTemplate($pdf_file, 0, 0, $format[0], $format[1]);
+          } catch (Exception $e) {
+            $file_name = basename($file);
+            $mpdf->WriteHTML("<div style='padding:20px;color:red;'><b>{$file_name}</b> file is invalid or not supported.</div>");
+          }
+        }
+      }
+    }
+
+    // Output
+    if (!$output) {
+      return $mpdf->Output();
+    } elseif ($output == 'INLINE') {
+      return $mpdf->Output($file_name . ".pdf", \Mpdf\Output\Destination::INLINE);
+    } elseif ($output == 'DOWNLOAD') {
+      return $mpdf->Output($file_name . ".pdf", \Mpdf\Output\Destination::DOWNLOAD);
+    } elseif ($output == "FILE") {
+      return $mpdf->Output("{$dest}{$file_name}.pdf", \Mpdf\Output\Destination::FILE);
+    }
+  }
+}
--- README.md
+++ README.md
@@ -1,2 +1,126 @@
 # MakePDF
-ProcessWire PDF generation module using mPDF library.
+
+ProcessWire PDF generation module using mPDF library. 
+
+### HTML 2 PDF
+Use this method to simply convert any html mark into pdf file.
+
+```php
+// Get the module
+$pdf = $modules->get('MakePDF');
+// Set the options
+$options = [];
+// Generate PDF
+$pdf->html2pdf("<h1>hello world</h1>", $options);
+```
+
+### GeneratePDF
+Use this method to generate multi-page PDF
+```php
+// Get the module
+$pdf = $modules->get('MakePDF');
+
+// Set the pages
+$pdf_pages = [
+  'page_1' => [
+    'tmpl' => __DIR__ . "/pdf/page-1.php",
+    'title' => "Page 1",
+  ],
+  'page_2' => [
+    'tmpl' => __DIR__ . "/pdf/page-2.php",
+    'title' => "Page 2",
+  ],
+];
+
+// Set the options
+$options = [
+  "font" => "mono",
+  "header" => "<div style='background: #f8f8f8;padding: 30px;'>Header</div>",
+  "footer" => "<div style='background: #f8f8f8;padding: 30px;'>Footer</div>",
+  "margin_top" => 60,
+  "margin_bottom" => 60,
+];
+
+// Generate PDF
+$pdf->generatePDF($pdf_pages, $options);
+
+
+```
+
+### Options
+| Option | Description | Default Value |
+|--------|-------------|---------------|
+| `mode` | The encoding mode of the generated PDF file. | `utf-8` |
+| `format` | The format of the generated PDF file. Can be either `[210, 297]` or `[612, 792]`. | `[210, 297]` |
+| `orientation` | The orientation of the generated PDF file. Can be either `P` (portrait) or `L` (landscape). | `P` |
+| `margin_top` | The top margin of the PDF file. | `20` |
+| `margin_bottom` | The bottom margin of the PDF file. | `20` |
+| `margin_left` | The left margin of the PDF file. | `20` |
+| `margin_right` | The right margin of the PDF file. | `20` |
+| `margin_header` | The header margin of the PDF file. | `20` |
+| `margin_footer` | The footer margin of the PDF file. | `20` |
+| `output` | The output mode of the generated PDF file. Can be either `INLINE` or `DOWNLOAD`. | `INLINE` |
+| `dest` | The destination path of the generated PDF file. | The temporary directory path |
+| `file_name` | The filename of the generated PDF file. | The current timestamp |
+| `header` | The HTML code for the header of the PDF file. | An empty string |
+| `footer` | The HTML code for the footer of the PDF file. | An empty string |
+| `font` | The font family of the generated PDF file. Can be either `sans`, `condensed`, `serif`, or `slab`. | `sans` |
+| `debug` | Whether to enable debug mode. | `false` |
+
+### CSS
+Modules includes basic css utility classes that you can use to style your pdf.
+
+| Class | Description |
+|-------|-------------|
+| `.align_center`, `.text-center` | Centers text. |
+| `.align_left`, `.text-left` | Aligns text to the left. |
+| `.align_justify`, `.text-justify` | Justifies text. |
+| `.align_right`, `.text-right` | Aligns text to the right. |
+| `.bg-muted` | Sets the background color to a muted gray. |
+| `.bg-white` | Sets the background color to white. |
+| `.color-white` | Sets the color of text to white. |
+| `.color-white-light` | Sets the color of text to a light white. |
+| `.float-left` | Floats an element to the left. |
+| `.float-right` | Floats an element to the right. |
+| `.line-height-1` | Sets the line height to 1. |
+| `.line-height-15` | Sets the line height to 1.5. |
+| `.list` | Removes the default list styles and sets the padding and margin to 0. |
+| `.margin` | Sets the margin to 20px on top and bottom, and 0px on left and right. |
+| `.margin-auto` | Centers an element horizontally. |
+| `.margin-bottom` | Sets the bottom margin to 20px. |
+| `.margin-remove` | Removes all margins. |
+| `.margin-remove-bottom` | Removes the bottom margin. |
+| `.margin-remove-top` | Removes the top margin. |
+| `.margin-small` | Sets the margin to 10px on top and bottom, and 0px on left and right. |
+| `.margin-small-bottom` | Sets the bottom margin to 10px. |
+| `.margin-small-top` | Sets the top margin to 10px. |
+| `.margin-top` | Sets the top margin to 20px. |
+| `.padding` | Sets the padding to 20px. |
+| `.padding-small` | Sets the padding to 10px. |
+| `.table` | Sets the styles of a table. |
+| `.table-reset` | Resets the styles of a table. |
+| `.text-bold` | Sets the font weight to bold. |
+| `.text-down` | Converts text to lowercase. |
+| `.text-italic` | Sets the font style to italic. |
+| `.text-large` | Sets the font size to 1.2em. |
+| `.text-lite` | Sets the font weight to light. |
+| `.text-muted` | Sets the color of text to a muted gray. |
+| `.text-rotate-90` | Rotates text 90 degrees. |
+| `.text-small` | Sets the font size to 0.8em. |
+| `.text-underlined` | Underlines text. |
+| `.text-up` | Converts text to uppercase. |
+| `.v-align-bottom` | Vertically aligns an element to the bottom. |
+| `.v-align-middle` | Vertically aligns an element to the middle. |
+| `.v-align-top` | Vertically aligns an element to the top. |
+| `.width-10` | Sets the width to 10%. |
+| `.width-100` | Sets the width to 100%. |
+| `.width-20` | Sets the width to 20%. |
+| `.width-30` | Sets the width to 30%. |
+| `.width-40` | Sets the width to 40%. |
+| `.width-48` | Sets the width to 48%. |
+| `.width-49` | Sets the width to 49%. |
+| `.width-50` | Sets the width to 50%. |
+| `.width-60` | Sets the width to 60%. |
+| `.width-70` | Sets the width to 70%. |
+| `.width-80` | Sets the width to 80%. |
+| `.width-90` | Sets the width to 90%. |
